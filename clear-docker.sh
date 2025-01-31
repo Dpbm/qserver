b70d162c494d3fc7a3ea7fffa1c97f63d8647ead
Dpbm <dpbm136@gmail.com>
2025-01-31 11:04:32 -0300

    added rabbitmq

--- clear-docker.sh
+++ clear-docker.sh
@@ -5,8 +5,13 @@
 BLUE='\033[0;34m'
 ENDC='\033[0m'
 
-echo -e "\n${BLUE}Stopping containers ... ${ENDC}"
-docker stop $(docker container ls -a | grep local-quantum-server-db | awk '{printf $1}')
+
+DB_CONTAINER_ID=$(docker container ls -a | grep local-quantum-server-db | awk '{printf $1}')
+
+if [ $DB_CONTAINER_ID ]; then
+	echo -e "\n${BLUE}Stopping container $DB_CONTAINER_ID ... ${ENDC}"
+	docker stop $DB_CONTAINER_ID
+fi
 
 echo -e "\n${BLUE}Cleaning containers ... ${ENDC}"
 yes | docker container prune
@@ -14,3 +19,5 @@ echo -e "\n${BLUE}Cleaning images ... ${ENDC}"
 yes | docker image prune -a
 echo -e "\n${BLUE}Cleaning volumes ... ${ENDC}"
 yes | docker volume prune -a
+echo -e "\n${BLUE}Cleaning networks ... ${ENDC}"
+yes | docker network prune
