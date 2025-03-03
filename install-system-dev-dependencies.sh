7e80de00006681f3cfd33e3ec0580d21ee158158
Dpbm <dpbm136@gmail.com>
2025-03-03 13:31:16 -0300

    updated nginx added healthcheck

---
+++ install-system-dev-dependencies.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e
+
+sudo apt update
+
+if [ ! $(which curl) &>/dev/null ]; then 
+	echo -e "Installing curl..."
+	sudo apt install curl -y
+fi
+
+if [ ! $(which go) &>/dev/null ]; then
+    echo -e "Installing golang..."
+
+    TAR_FILE="go1.23.5.linux-amd64.tar.gz"
+    TARGET_GO_TAR_FILE="go.tar.gz"
+    TARGET_GO_TAR_PATH="/tmp/$TARGET_GO_TAR_FILE"
+    GO_VERSION_URL="https://go.dev/dl/$TAR_FILE"
+
+    curl -L "$GO_VERSION_URL" -o "$TARGET_GO_TAR_PATH"
+
+    tar -C /tmp -xvf "$TARGET_GO_TAR_FILE"
+    mv ./go "$HOME"
+    rm -rf "$TARGET_GO_TAR_FILE"
+
+    echo "export PATH=\$HOME/go/bin:\$PATH" >> "$HOME/.bashrc"
+    source "$HOME/.bashrc"
+fi
+
+
+if [ ! $(which grpcurl) &>/dev/null ]; then
+    echo -e "Installing grpcurl..."
+    go install github.com/fullstorydev/grpcurl/cmd/grpcurl@v1.9.2
+fi
--- proxy/test.sh
+++ proxy/test.sh
@@ -5,20 +5,9 @@ set -e
 SERVER_IP=172.18.0.30
 SERVER_PORT=8080
 
-echo "Testing NGINX Routes..."
+source ../colors.sh
 
-
-if [ ! $(which curl) &>/dev/null ]; then 
-	echo -e "Installing curl..."
-	sudo apt update
-	sudo apt install curl -y
-fi
-
-if [ ! $(which grpcurl) &>/dev/null ]; then
-    echo -e "Installing grpcurl..."
-    go install github.com/fullstorydev/grpcurl/cmd/grpcurl@v1.9.2
-fi
-
+echo -e "${BLUE}Testing NGINX Routes...${ENDC}"
 
 test_status(){
     URL=$1
@@ -26,10 +15,10 @@ test_status(){
     GET_STATUS_CODE=$(curl -so /dev/null -w '%{response_code}' $URL)
 
     if [ $GET_STATUS_CODE != $DESIRED_STATUS_CODE ]; then
-        echo "Error: status $GET_STATUS_CODE for url $URL instead of $DESIRED_STATUS_CODE"
+        echo -e "${RED}Error: status $GET_STATUS_CODE for url $URL instead of $DESIRED_STATUS_CODE${ENDC}"
         exit 1
     else
-        echo "Passed: $URL"
+        echo -e "${GREEN}Passed: $URL${ENDC}"
     fi
 }
 
@@ -39,7 +28,7 @@ add_plugin(){
     STATUS_CODE=$(curl --request POST -so /dev/null -w '%{response_code}' "$BASE_URL/api/v1/plugin/$DEFAULT_PLUGIN")
 
     if [ $STATUS_CODE != 201 ]; then
-        echo "Failed on add plugin!"
+        echo -e "${RED}Failed on add plugin!${ENDC}"
         exit 1
     fi
 }
@@ -59,18 +48,19 @@ EOM
 SERVER=$SERVER_IP:$SERVER_PORT
 BASE_URL="http://$SERVER"
 
-echo "--Test API Access--"
+echo -e "${BLUE}--Test API Access--${ENDC}"
 test_status "$BASE_URL/api/v1/jobs/" 200
 test_status "$BASE_URL/api/not-exists/" 404
 
-echo "--Test Swagger--"
+echo -e "${BLUE}--Test Swagger--${ENDC}"
 test_status "$BASE_URL/swagger/" 200
 test_status "$BASE_URL/swagger/index.html" 200
 test_status "$BASE_URL/swagger/anything" 200
 
-echo "--Test NGINX--"
+echo -e "${BLUE}--Test NGINX--${ENDC}"
 test_status "$BASE_URL/not-a-nginx-route/" 415
+test_status "$BASE_URL/healthcheck/" 200
 
-echo "--Test GRPC--"
+echo -e "${BLUE}--Test GRPC--${ENDC}"
 add_plugin $BASE_URL
 send_grpc $SERVER
