7fac1f9322fdb49f1ee215c545d08af5509c6adc
Dpbm <dpbm136@gmail.com>
2025-03-05 14:50:30 -0300

    fixed comparison

--- check-postgres-status-for-ci.sh
+++ check-postgres-status-for-ci.sh
@@ -7,13 +7,13 @@ source ./colors.sh
 RUNNING_STATUS=$(docker inspect postgres-db | jq '.[].State.Status')
 HEALTH_STATUS=$(docker inspect postgres-db | jq '.[].State.Health.Status')
 
-if [ $RUNNING_STATUS != 'running' ]; then
+if [ "$RUNNING_STATUS" != '"running"' ]; then
     echo -e "${RED}Running Status is: ${RUNNING_STATUS}${ENDC}"
     exit 1;
 fi
 
 
-if [ $HEALTH_STATUS != 'healthy' ]; then
+if [ "$HEALTH_STATUS" != '"healthy"' ]; then
     echo -e "${RED}Health Status is: ${HEALTH_STATUS}${ENDC}"
     exit 1;
 fi
