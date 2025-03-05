e9a68fa8331116fc00b43e666c92123e09665e0c
Dpbm <dpbm136@gmail.com>
2025-03-05 15:02:02 -0300

    updated script to check starting status

--- check-postgres-status-for-ci.sh
+++ check-postgres-status-for-ci.sh
@@ -5,13 +5,18 @@ set -e
 source ./colors.sh
 
 RUNNING_STATUS=$(docker inspect postgres-db | jq '.[].State.Status')
-HEALTH_STATUS=$(docker inspect postgres-db | jq '.[].State.Health.Status')
 
 if [ "$RUNNING_STATUS" != '"running"' ]; then
     echo -e "${RED}Running Status is: ${RUNNING_STATUS}${ENDC}"
     exit 1;
 fi
 
+STARTING_STATUS='"starting"'
+HEALTH_STATUS="$STARTING_STATUS"
+while [ "$HEALTH_STATUS" == "$STARTING_STATUS" ]
+do
+    HEALTH_STATUS=$(docker inspect postgres-db | jq '.[].State.Health.Status')
+done
 
 if [ "$HEALTH_STATUS" != '"healthy"' ]; then
     echo -e "${RED}Health Status is: ${HEALTH_STATUS}${ENDC}"
