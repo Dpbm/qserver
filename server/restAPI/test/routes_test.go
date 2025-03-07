d05b796d81d20615166ce0926f3692fadb661ed6
Dpbm <dpbm136@gmail.com>
2025-03-06 21:56:19 -0300

    start testing

--- server/restAPI/test/routes_test.go
+++ server/restAPI/test/routes_test.go
@@ -7,18 +7,130 @@ import (
 	"testing"
 
 	"github.com/DATA-DOG/go-sqlmock"
-	constants "github.com/Dpbm/quantumRestAPI/constants"
+	constnats "github.com/Dpbm/quantumRestAPI/constants"
 	"github.com/Dpbm/quantumRestAPI/db"
 	"github.com/Dpbm/quantumRestAPI/server"
 	dbDefinition "github.com/Dpbm/shared/db"
 	"github.com/stretchr/testify/assert"
 )
 
+// TEST DELETE PLUGIN (WITHOUT ANY JOB ASSIGNED TO IT, on HISTORY)
+// TEST DELETE WITH JOBS USING IT
+
 const dbHost = ""
 const dbPort = 0
 const dbUsername = ""
 const dbPassword = ""
 const dbName = ""
+
+// ------- ADD PLUGIN -------
+func TestAddPluginFailedNoPluginName(t *testing.T) {
+	dbInstance := db.DB{}
+	dbInstance.Connect(&dbDefinition.Mock{}, dbHost, dbPort, dbUsername, dbPassword, dbName)
+	defer dbInstance.CloseConnection()
+
+	server := server.SetupServer(&dbInstance)
+
+	writer := httptest.NewRecorder()
+	req, _ := http.NewRequest("POST", "/api/v1/plugin/", nil)
+
+	server.ServeHTTP(writer, req)
+
+	assert.Equal(t, 404, writer.Code)
+}
+
+func TestAddPluginFailedInvalidPlugin(t *testing.T) {
+	dbInstance := db.DB{}
+	dbInstance.Connect(&dbDefinition.Mock{}, dbHost, dbPort, dbUsername, dbPassword, dbName)
+	defer dbInstance.CloseConnection()
+
+	server := server.SetupServer(&dbInstance)
+
+	writer := httptest.NewRecorder()
+	req, _ := http.NewRequest("POST", "/api/v1/plugin/invalid-plugin-test-it-should-not-work", nil)
+
+	server.ServeHTTP(writer, req)
+
+	assert.Equal(t, 500, writer.Code)
+}
+
+func TestAddPluginSuccess(t *testing.T) {
+	dbInstance := db.DB{}
+	dbInstance.Connect(&dbDefinition.Mock{}, dbHost, dbPort, dbUsername, dbPassword, dbName)
+	defer dbInstance.CloseConnection()
+
+	server := server.SetupServer(&dbInstance)
+
+	mock, ok := dbInstance.Extra.(sqlmock.Sqlmock)
+	if !ok {
+		t.Fatal("Failed on parse mock")
+	}
+
+	mock.ExpectExec("INSERT INTO backends").WithArgs("aer", "aer-plugin").WillReturnResult(sqlmock.NewResult(1, 1))
+
+	writer := httptest.NewRecorder()
+	req, _ := http.NewRequest("POST", fmt.Sprintf("/api/v1/plugin/%s", constnats.TEST_PLUGIN), nil)
+
+	server.ServeHTTP(writer, req)
+
+	assert.Equal(t, 201, writer.Code)
+}
+
+// ------- ADD PLUGIN -------
+
+func TestDeletePluginFailedNoPluginName(t *testing.T) {
+	dbInstance := db.DB{}
+	dbInstance.Connect(&dbDefinition.Mock{}, dbHost, dbPort, dbUsername, dbPassword, dbName)
+	defer dbInstance.CloseConnection()
+
+	server := server.SetupServer(&dbInstance)
+
+	writer := httptest.NewRecorder()
+	req, _ := http.NewRequest("DELETE", "/api/v1/plugin/", nil)
+
+	server.ServeHTTP(writer, req)
+
+	assert.Equal(t, 404, writer.Code)
+}
+
+func TestDeletePluginFailedNotFoundInstalledPlugin(t *testing.T) {
+	dbInstance := db.DB{}
+	dbInstance.Connect(&dbDefinition.Mock{}, dbHost, dbPort, dbUsername, dbPassword, dbName)
+	defer dbInstance.CloseConnection()
+
+	server := server.SetupServer(&dbInstance)
+
+	writer := httptest.NewRecorder()
+	req, _ := http.NewRequest("DELETE", "/api/v1/plugin/doesnt-exists", nil)
+
+	server.ServeHTTP(writer, req)
+
+	assert.Equal(t, 404, writer.Code)
+}
+
+func TestDeletePluginSuccess(t *testing.T) {
+	dbInstance := db.DB{}
+	dbInstance.Connect(&dbDefinition.Mock{}, dbHost, dbPort, dbUsername, dbPassword, dbName)
+	defer dbInstance.CloseConnection()
+
+	server := server.SetupServer(&dbInstance)
+
+	mock, ok := dbInstance.Extra.(sqlmock.Sqlmock)
+	if !ok {
+		t.Fatal("Failed on parse mock")
+	}
+
+	mock.ExpectExec("DELETE FROM backends").WithArgs("aer-plugin").WillReturnResult(sqlmock.NewResult(1, 1))
+
+	writer := httptest.NewRecorder()
+	req, _ := http.NewRequest("DELETE", "/api/v1/plugin/aer-plugin", nil)
+
+	server.ServeHTTP(writer, req)
+
+	assert.Equal(t, 200, writer.Code)
+}
+
+/*
 
 func TestGetNoJobId(t *testing.T) {
 	dbInstance := db.DB{}
@@ -177,3 +289,4 @@ func TestPotCorrectPlugin(t *testing.T) {
 	assert.Equal(t, 201, writer.Code)
 
 }
+*/
---
+++ server/restAPI/test/usage-tests.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+set +e
+
+source ../../../colors.sh
+source ../../../test-utils.sh
+
+SERVER_URL="$HOST:3000"
+DEFAULT_PLUGIN="aer-plugin"
+GRPC_SERVER="0.0.0.0:50051"
+
+add_plugin(){
+    curl --request POST -f "$SERVER_URL/api/v1/plugin/$DEFAULT_PLUGIN"
+
+    if [ $? != 0 ]; then
+        echo -e "${RED}Failed on add plugin${ENDC}"
+        return 1
+    fi
+}
+
+add_job(){
+        DATA=$(cat <<EOM
+{"properties":{"resultTypeCounts":false, "resultTypeQuasiDist":true, "resultTypeExpVal":false, "targetSimulator":"aer", "metadata":"{}"}}
+{"qasmChunk":"AAAA"}
+EOM
+)
+    grpcurl -plaintext -d "$DATA" $GRPC_SERVER Jobs/AddJob
+
+    if [ $? != 0 ]; then
+        echo -e "${RED}Failed on add job${ENDC}"
+        return 1
+    fi
+}
+
+delete_plugin(){
+    curl --request DELETE -f "$SERVER_URL/api/v1/plugin/$DEFAULT_PLUGIN"
+    if [ $? != 0 ]; then
+        echo -e "${RED}Failed on delete plugin${ENDC}"
+        return 1
+    fi
+}
+
+run_test_1(){
+    add_plugin
+    if [ $? != 0 ]; then
+        return 1
+    fi
+    echo ""
+
+    delete_plugin
+    echo ""
+}
+
+
+run_test_2(){
+    add_plugin
+    if [ $? != 0 ]; then
+        return 1
+    fi
+    echo ""
+
+    add_job
+    if [ $? != 0 ]; then
+        return 1
+    fi
+    echo ""
+
+
+    delete_plugin
+    if [ $? != 0 ]; then
+        return 0
+    fi
+}
+
+
+test_header 1 "Delete plugin with no job created with it"
+run_test_1
+has_passed
+
+test_header 2 "Delete plugin with no job created with it"
+run_test_2
+has_passed
