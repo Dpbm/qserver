0fc6ce7cf65b8bb0a510cfaf2658a37b157892b7
Dpbm <dpbm136@gmail.com>
2025-04-08 14:41:47 -0300

    fixing ssl command

--- certs/generate-ci-certs.sh
+++ certs/generate-ci-certs.sh
@@ -11,7 +11,7 @@ echo -e "${GREEN}Generating certs for $DOMAIN self signed ${ENDC}"
 # from: https://letsencrypt.org/docs/certificates-for-localhost/ 
 openssl req -x509 -out fullchain1.pem -keyout privkey1.pem \
   -newkey rsa:2048 -nodes -sha256 \
-  -subj '/CN=localhost' -extensions EXT -config <( \
+  -subj '/CN=$DOMAIN' -extensions EXT -config <( \
    printf "[dn]\nCN=%s\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:%s\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth" $DOMAIN $DOMAIN)
 
 TARGET_DIR="/etc/letsencrypt/archive/$DOMAIN"
