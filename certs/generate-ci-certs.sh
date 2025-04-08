81abdfc40cf8daff53dacd436d7b9f6b773db936
Dpbm <dpbm136@gmail.com>
2025-04-08 14:39:36 -0300

    fixing printf

--- certs/generate-ci-certs.sh
+++ certs/generate-ci-certs.sh
@@ -7,10 +7,12 @@ source ../colors.sh
 DOMAIN=$1
 
 echo -e "${GREEN}Generating certs for $DOMAIN self signed ${ENDC}"
+
+# from: https://letsencrypt.org/docs/certificates-for-localhost/ 
 openssl req -x509 -out fullchain1.pem -keyout privkey1.pem \
   -newkey rsa:2048 -nodes -sha256 \
   -subj '/CN=localhost' -extensions EXT -config <( \
-   printf "[dn]\nCN=$DOMAIN\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:$DOMAIN\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
+   printf "[dn]\nCN=%s\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:%s\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth" $DOMAIN $DOMAIN)
 
 TARGET_DIR="/etc/letsencrypt/archive/$DOMAIN"
 echo -e "${GREEN}Moving certs to ${TARGET_DIR}${ENDC}"
